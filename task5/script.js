27083a1efafbcd26fc785ce8d1391ad0eaedaa04
MAV1703 <nastya.aleinowa@yandex.ru>
2025-07-02 21:30:02 +0400

    Add files via upload

--- task5/script.js
+++ task5/script.js
@@ -1,31 +1,18 @@
+let buttons = []
 const imgBtn1 = document.querySelector("#btn1")
+buttons.push(imgBtn1)
 const imgBtn2 = document.querySelector("#btn2")
+buttons.push(imgBtn2)
 const imgBtn3 = document.querySelector("#btn3")
+buttons.push(imgBtn3)
 const imgBtn4 = document.querySelector("#btn4")
+buttons.push(imgBtn4)
 const imgBtn5 = document.querySelector("#btn5")
+buttons.push(imgBtn5)
 const bigImg = document.querySelector(".mainPicture")
 
-imgBtn1.addEventListener("click", ()=>{
-    bigImg.src = imgBtn1.src
+buttons.forEach ((element) => {element.addEventListener("click", ()=>{
+    bigImg.src = element.src
+    
+})});
 
-})
-
-imgBtn2.addEventListener("click", ()=>{
-    bigImg.src = imgBtn2.src
-
-})
-
-imgBtn3.addEventListener("click", ()=>{
-    bigImg.src = imgBtn3.src
-
-})
-
-imgBtn4.addEventListener("click", ()=>{
-    bigImg.src = imgBtn4.src
-
-})
-
-imgBtn5.addEventListener("click", ()=>{
-    bigImg.src = imgBtn5.src
-
-})
